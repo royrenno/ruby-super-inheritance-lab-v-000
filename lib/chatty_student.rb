c62c9809bf1a7806991018df4ee81ba1236031b6
roy renno <royrenno@yahoo.com>
2019-05-10 23:11:10 +0000

    Automatically backed up by Learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -3,7 +3,7 @@ class ChattyStudent < Student
   def hello
     super
     puts "How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night?! Oh man,
-    it was so crazy! What, you don't want any spoilers! Okay well let me just tell you who died..."
+    it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died..."
   end
 
   def raise_hand
